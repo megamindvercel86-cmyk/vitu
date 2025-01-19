76e239eb9e370dbd15ac15f8ab2ac143677acd44
Jamshadj <jamshadalij2004@gmail.com>
2025-01-19 18:44:35 +0530

    colour updated

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -35,7 +35,7 @@ const FAQ = () => {
   ];
 
   return (
-    <section className="bg-primaryBackgroundColor mt-[200px]">
+    <section className="bg-white mt-[200px]">
        <div className="flex flex-col lg:flex-row pt-[66px] px-[27px] md:pt-[66px] lg:pt-[155px] xl:pt-[159px] xl:px-[277px] lg:px-[132px] gap-[30px] md:gap-[45px]">
         {/* Left Section */}
         <div className="flex-1">
