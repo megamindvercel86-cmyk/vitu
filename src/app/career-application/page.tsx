a7189417e20f64957801fdb273ad3ce0cdb2221d
jamshadj <jamshadalij2004@gmail.com>
2025-01-24 16:38:42 +0530

    career page updated

--- src/app/career-application/page.tsx
+++ src/app/career-application/page.tsx
@@ -1,25 +1,15 @@
 "use client";
 
-
 import FormSection from "@/components/FormSection/FormSection";
 import Layout from "@/components/Layout/Layout";
 
 export default function CareerApplication() {
   return (
-     <Layout navbarProps={{ showGetInTouch: false }}>
+    <Layout navbarProps={{ showGetInTouch: false }}>
       <FormSection
-        heading={
-          <>
-            The career you've been 
-            waiting for starts here!
-          </>
-        }
-        subheading={
-          <>
-            Begin your journey to a rewarding career—fill 
-            out the form & let’s get started
-          </>
-        }
+        heading="The career you've been waiting for starts here! "
+        subheading="Begin your journey to a rewarding career—fill 
+                    out the form & let’s get started"
         page="Career Application"
       />
     </Layout>
