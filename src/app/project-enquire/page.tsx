ff2c75fdbdea8f90040ff02b58bff164f6a297e4
jamshadj <jamshadalij2004@gmail.com>
2025-01-24 16:34:10 +0530

    project enquire txt updated

--- src/app/project-enquire/page.tsx
+++ src/app/project-enquire/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import FormSection from "@/components/FormSection/FormSection";
 import Layout from "@/components/Layout/Layout";
 
@@ -6,18 +6,11 @@ export default function ProjetEnquire() {
   return (
     <Layout navbarProps={{ showGetInTouch: false }}>
       <FormSection
-        heading={
-          <>
-           Your dream home is 
-           closer than you think!
-          </>
-        }
-        subheading={
-          <>
+        heading="Your dream home is closer than you think!"
+        subheading="
             Begin your journey to a new home—fill out the 
             form & let’s get started.
-          </>
-        }
+          "
         page="Project Enquire"
       />
     </Layout>
