a4979db011e09da2edec937a826c55a9aa251a22
Jamshadj <jamshadalij2004@gmail.com>
2025-01-27 21:39:08 +0530

    removed toaster

--- src/app/general-enquire/page.tsx
+++ src/app/general-enquire/page.tsx
@@ -3,12 +3,9 @@
 import FAQ from "@/components/FAQ/FAQ";
 import FormSection from "@/components/FormSection/FormSection";
 import Layout from "@/components/Layout/Layout";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 export default function GeneralEnquire() {
   return (
     <Layout navbarProps={{ showGetInTouch: false }}>
-      <ToastContainer position="top-right" autoClose={5000} />
       <FormSection
         heading="Excited about the possibilities your next home could offer?"
         subheading="
