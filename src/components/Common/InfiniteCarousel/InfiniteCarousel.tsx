75b8c02b7fed2dfe70946ab73d5ff38246624f6a
viturealty <laxmankamath123@gmail.com>
2025-02-28 15:06:33 +0530

    mobile carousel content added

--- src/components/Common/InfiniteCarousel/InfiniteCarousel.tsx
+++ src/components/Common/InfiniteCarousel/InfiniteCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import type { Swiper as SwiperType } from 'swiper';
 import "swiper/css";
@@ -9,11 +9,14 @@ import { Autoplay, EffectCoverflow, Navigation } from "swiper/modules";
 import "./InfiniteCarousel.css";
 import AppleStyleCard from "@/components/ui/apple-style-card";
 import {
+  ArrowRightIcon,
   IconArrowNarrowLeft,
   IconArrowNarrowRight,
 } from "@/components/Icons/Icons";
 import Typography from "@/components/Typography/Typography";
-
+import exploreProjects from "@/data/exploreProjects.json";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
 // Define a type for the card object
 interface Card {
   id: number;
@@ -35,6 +38,99 @@ interface Card {
 interface InfiniteCarouselProps {
   cards: Card[];
 }
+
+interface FooterProps {
+  onFooterClick: () => void;
+  nextProjectTitle: string;
+}
+
+const Footer: React.FC<FooterProps> = ({ onFooterClick, nextProjectTitle }) => {
+  console.log("FooterProps", nextProjectTitle);
+
+  return (
+    <div className="bg-white rounded-b-xl lg:rounded-b-3xl pt-10 lg:pb-0">
+      <hr className="w-full h-[2px] bg-[#BDBEC2]" />
+      <div
+        onClick={onFooterClick}
+        className="px-0  container gap-8 lg:gap-48 flex justify-between lg:justify-between items-center py-2 lg:py-12 cursor-pointer"
+      >
+        <div>
+          <p className="text-sm font-FreightNeoProNormal font-bold text-[#8E8E93] ">
+            UP NEXT
+          </p>
+          <h4 className=" font-bold text-lg font-FreightNeoProBold max-w-[15rem] text-[#1D1D1F] lg:max-w-none">
+            {nextProjectTitle}
+          </h4>
+        </div>
+        <ArrowRightIcon />
+      </div>
+    </div>
+  );
+};
+
+// Update the CardContent component to accept props
+const CardContent = ({ cardId }: { cardId: number }) => {
+  const [currentCardId, setCurrentCardId] = useState(cardId);
+
+  let project = exploreProjects.find((project) => project.id === currentCardId);
+
+  const handleFooterClick = () => {
+    console.log(
+      "Footer clicked - navigate to next project or perform other action"
+    );
+
+    const nextProject = exploreProjects.find((project) => {
+      if (project.id === 5) {
+        return 1 === currentCardId;
+      } else {
+        return project.id + 1 === currentCardId;
+      }
+    });
+
+    if (nextProject) {
+      setCurrentCardId(nextProject.id); // Update state to trigger re-render
+    }
+  };
+
+  const nextProject = exploreProjects.find((project) => {
+    if (project.id === 5) {
+      return 1 === currentCardId;
+    } else {
+      return project.id + 1 === currentCardId;
+    }
+  });
+
+  console.log(nextProject);
+
+  return (
+    <>
+      {project && (
+        <div key={"dummy-content"}>
+          <Image
+            src={project.url || "/placeholder.svg"}
+            alt={nextProject?.title || "Card image"}
+            width={1042}
+            height={45}
+            className={cn("object-   h-[652px] w-full")}
+          />
+          <div className="p-4 md:p-10">
+
+          <Typography variant="h1" className="text-customBrown">
+            {project.title}
+          </Typography>
+          <Typography className="text-[#04070799] font-FreightNeoProNormal pt-[20px] !text-xl">
+            {project.introduction}
+          </Typography>
+          <Footer
+            onFooterClick={handleFooterClick}
+            nextProjectTitle={nextProject?.title || ""}
+          />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 const InfiniteCarousel: React.FC<InfiniteCarouselProps> = ({ cards }) => {
   const swiperRef = useRef<SwiperType | undefined>(undefined);
@@ -94,6 +190,7 @@ const InfiniteCarousel: React.FC<InfiniteCarouselProps> = ({ cards }) => {
               subtitle={card.subtitle}
               category={card.category}
               isViewMore={card.isViewMore}
+              content={<CardContent cardId={card.id} />}
             />
             <Typography variant="custom"> {card.name}</Typography>
           </SwiperSlide>
