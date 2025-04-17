5456b6de0d10e68fe87c7432da82c6f615847e0d
Jamshadj <jamshadalij2004@gmail.com>
2025-04-17 11:41:23 +0530

    updated email

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
     // Send email
     await transporter.sendMail({
       from: `${email}`,
-      to: "developer.megamind@gmail.com", // Replace with the recipient's email
+      to: "mmstudio.presentation@gmail.com", // Replace with the recipient's email
       subject,
       html: emailTemplate,
     });
