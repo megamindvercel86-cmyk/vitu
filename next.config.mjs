d95ef7a47731e6d2687d997324e833215814ad7d
Jamshadj <jamshadalij2004@gmail.com>
2025-05-09 16:22:33 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+ 
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true, 
-  },
-  output: 'export',
-  
-  trailingSlash: true,
+  }
 };
 
 export default nextConfig;
