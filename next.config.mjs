2895be96e0bc7d7c6bfac1889f04fc4ebb177c1d
Jamshadj <jamshadalij2004@gmail.com>
2025-05-09 16:14:10 +0530

    updated the imafe

--- next.config.mjs
+++ next.config.mjs
@@ -1,69 +1,17 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        pathname: "/v0/b/**", // Allows any Firebase Storage bucket
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
+    unoptimized: true,
   },
-
-  async redirects() {
-    return [
-      { source: "/properties", destination: "/projects", permanent: true },
-      { source: "/mangalore-real-estate", destination: "/", permanent: true },
-    ];
-  },
-
-  compress: true,
-  productionBrowserSourceMaps: false,
-
-  headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [
-        { key: "X-DNS-Prefetch-Control", value: "on" },
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=63072000; includeSubDomains; preload",
-        },
-        { key: "X-Frame-Options", value: "SAMEORIGIN" },
-        { key: "X-Content-Type-Options", value: "nosniff" },
-        { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        { key: "X-Robots-Tag", value: "index, follow" },
-      ],
-    },
-  ],
-
-  experimental: {
-    optimizePackageImports: ["framer-motion", "swiper"],
-  },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, 
   },
+  output: 'export',
+  
+  trailingSlash: true,
 };
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
