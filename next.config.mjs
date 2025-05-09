767b53adc0032fb1a4e29fc90861ada9a9286889
Jamshadj <jamshadalij2004@gmail.com>
2025-05-09 16:24:08 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/**", // Allows any Firebase Storage bucket
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
