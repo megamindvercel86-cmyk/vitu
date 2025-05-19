07838622bfbf7bea4e2c6245af5ddbf24b4c3b4a
Hashimhx <hashimhx3@gmail.com>
2025-05-19 14:38:14 +0530

    first (#89)

--- src/components/VilasamProjectPage/CurrentProject/CurrentProject.tsx
+++ src/components/VilasamProjectPage/CurrentProject/CurrentProject.tsx
@@ -573,7 +573,7 @@ const CurrentProject: React.FC = () => {
 
   return (
     <section
-      className="flex flex-col sm:flex-col  lg:flex-row mx-[1rem] sm:mx-[1rem] md:mx-[4.125rem] lg:mx-[3.5rem] xl:mx-[9rem]"
+      className="flex flex-col sm:flex-col lg:max-h-screen  lg:flex-row mx-[1rem] sm:mx-[1rem] md:mx-[4.125rem] lg:mx-[3.5rem] xl:mx-[9rem]"
       aria-labelledby="project-title"
     >
       {/* Left Column - Project Details */}
