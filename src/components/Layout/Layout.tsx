ea52addee30eabcf1bf7cb99a9213f67921046e1
Jamshadj <jamshadalij2004@gmail.com>
2025-06-20 17:41:55 +0530

    updated the loader for performance

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import Footer from "./Footer";
@@ -9,7 +9,7 @@ import WhatsappChatWidget from "../Common/WhatsappChatWidget";
 import ScrollToTopButton from "../Common/ScrollToTopButton";
 import { usePathname } from "next/navigation";
 
-const Loader = dynamic(() => import("../loader"), { ssr: false });
+// const Loader = dynamic(() => import("../loader"), { ssr: false });
 
 type NavbarType = "primary" | "secondary";
 
@@ -25,19 +25,19 @@ interface LayoutProps {
 }
 
 export default function Layout({ children, navbarProps, navbarClassName }: LayoutProps) {
-  const [showLoader, setShowLoader] = useState(true);
-  const pathname = usePathname();
+  // const [showLoader, setShowLoader] = useState(true);
+  // const pathname = usePathname();
 
-  useEffect(() => {
-    setShowLoader(true); // Trigger loader again on route change
-  }, [pathname]);
+  // useEffect(() => {
+  //   setShowLoader(true); // Trigger loader again on route change
+  // }, [pathname]);
 
   return (
     <>
       <ToastContainer />
-      {showLoader && <Loader onFinish={() => setShowLoader(false)} />}
+      {/* {showLoader && <Loader onFinish={() => setShowLoader(false)} />} */}
 
-      <div className={`transition-opacity duration-700 ease-in-out ${showLoader ? "opacity-0" : "opacity-100"}`}>
+      <div >
         {/* Navbar */}
         <div className={navbarClassName || ""}>
           <Navbar {...navbarProps} />
