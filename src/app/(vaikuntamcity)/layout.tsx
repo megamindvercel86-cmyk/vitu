0b9f3cd4c920af5cda5d4cc5a49ee417894dc638
Jamshadj <jamshadalij2004@gmail.com>
2025-06-26 14:43:05 +0530

    updated the imagews

--- src/app/(vaikuntamcity)/layout.tsx
+++ src/app/(vaikuntamcity)/layout.tsx
@@ -1,12 +1,36 @@
+"use client"
+import dynamic from "next/dynamic";
 import ScrollToTopButton from "@/components/Common/ScrollToTopButton";
 import WhatsappChatWidget from "@/components/Common/WhatsappChatWidget";
 import ProjectFooter from "@/components/ProjectsPageComponents/ProjectsPageCommonComponents/ProjectPageFoooter/ProjectPageFooter";
 import ProjectNavbar from "@/components/ProjectsPageComponents/ProjectsPageCommonComponents/ProjectPageNavbar/ProjectPageNavbar";
-
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+const Loader = dynamic(() => import("../../components/loader"), { ssr: false });
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [showLoader, setShowLoader] = useState(true);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setShowLoader(true);
+
+    const timeout = setTimeout(() => {
+      setShowLoader(false); // Fade out after delay
+    }, 2500); // 2.5s loader time, adjust to match Lottie length
+
+    return () => clearTimeout(timeout);
+  }, [pathname]);
   return (
+    <>
+     <div
+        className={`fixed inset-0 bg-white z-50 flex justify-center items-center transition-opacity duration-700 ${
+          showLoader ? "opacity-1000 pointer-events-auto" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <Loader />
+      </div> 
     <div className="min-h-screen flex flex-col"> 
       <ProjectNavbar />
       <main className="flex-1">{children}</main>
@@ -14,5 +38,6 @@ export default function RootLayout({
       <WhatsappChatWidget/>
       <ProjectFooter />
     </div>
+    </>
   );
 }
