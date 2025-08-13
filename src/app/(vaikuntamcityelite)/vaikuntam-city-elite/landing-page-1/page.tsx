48f0744bc894df3c4ab9e814ac71500e6bac0c1c
Megamind <laxmankamath123@gmail.com>
2025-08-13 17:22:26 +0530

    feat:GTM tags added for homebuyers and investors

--- src/app/(vaikuntamcityelite)/vaikuntam-city-elite/landing-page-1/page.tsx
+++ src/app/(vaikuntamcityelite)/vaikuntam-city-elite/landing-page-1/page.tsx
@@ -20,9 +20,29 @@ import availablePlot2 from "../../../../../public/images/vaikuntamCityEliteLandi
 import availablePlot3 from "../../../../../public/images/vaikuntamCityEliteLandingPage/availablePlot3.webp";
 import NumberCounter2 from "@/components/VaikuntamCityElite/landing-page/NumberCounter2";
 import Features2 from "@/components/VaikuntamCityElite/landing-page/Features2";
+import Script from "next/script";
 export default function vaikuntamCityEliteLandingPage() {
   return (
     <>
+      <Script
+        id="gtm-pageB"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PCGZRLX5');`, // <-- different GTM ID
+        }}
+      />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PCGZRLX5"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       <EliteNavbar2 leftAlignLogos />
       <Hero
         leftAlign
@@ -33,7 +53,7 @@ export default function vaikuntamCityEliteLandingPage() {
           title2: "Fastest Growing Corridor",
         }}
       />
-       
+
       <OurElevatedVision2
         numberCounterData={numberCounterData2}
         title={ourElevatedVisionData.title}
@@ -96,7 +116,6 @@ const numberCounterData2 = [
     icon: "keyinhand",
   },
 ];
-
 
 const accordianData = [
   {
