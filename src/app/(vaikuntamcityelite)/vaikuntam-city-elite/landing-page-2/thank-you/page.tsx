1dc8c654f593ecbdb20759b036b64f708209f388
viturealty <laxmankamath123@gmail.com>
2025-08-30 00:26:07 +0530

    gtm tage updated

--- src/app/(vaikuntamcityelite)/vaikuntam-city-elite/landing-page-2/thank-you/page.tsx
+++ src/app/(vaikuntamcityelite)/vaikuntam-city-elite/landing-page-2/thank-you/page.tsx
@@ -2,6 +2,7 @@
 import AnimatedHeading from "@/components/VaikuntamCityElite/landing-page/AnimatedHeading";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import {  useRef, useState } from "react";
 
 export default function ThankYouPage() {
@@ -19,6 +20,17 @@ export default function ThankYouPage() {
 
   return (
     <>
+         <Script
+        id="gtm-pageB"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TX692SDK');`, // <-- different GTM ID
+        }}
+      />
       <section className=" bg-[#F3EAE1] relative z-[80] ">
         <div className="container px-12 lg:px-0 mx-auto lg:border-l lg:border-r border-[#1C1213] py-10 lg:py-16 h-full ">
           <img
