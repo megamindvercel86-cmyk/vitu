f20b9759e3cb967f7caf829a22ae8626adeb6624
megamindvercel86-cmyk <megamind-s-projects>
2026-05-29 18:07:40 +0530

    zoho done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -148,6 +148,7 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '929942736319525');
 fbq('track', 'PageView');
+
 `
           }}
         />
