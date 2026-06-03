95dbd2657bb5b37648e84500d57d914731e90afe
Jamshadj <developer@megamind.studio>
2026-06-03 14:33:03 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -135,8 +135,31 @@ export default function RootLayout({
         <meta name="facebook-domain-verification" content="y3e6dbvhw9o7jam62imr8ybuzu7bbw" />
         <meta name="robots" content="index, follow" />
         {/* Meta Pixel Code */}
-      
-       
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1009654298142393');
+fbq('track', 'PageView');
+`
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1009654298142393&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {/* End Meta Pixel Code */}
         {/* Zoho PageSense */}
         <script src="https://cdn-in.pagesense.io/js/viturealty/89122bda8676471491a62a43daad2492.js"></script>
